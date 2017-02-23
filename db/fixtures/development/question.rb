ad98b8a9a2c6bca5cd3aa58fdb4c7893f28ff23a
atsushi0623itoh <atsushi0623itoh@gmail.com>
2017-02-23 09:08:06 +0900

    create seed file of question

---
+++ db/fixtures/development/question.rb
@@ -0,0 +1,44 @@
+Question.seed(:id,
+  {
+    :id => 1,
+    :title => 'angular2 is fun？',
+    :content => <<~EOT
+  test test
+  EOT
+  },
+  {
+    :id => 2,
+    :title => 'rails5 is uesful?',
+    :content => <<~EOT
+    test test
+  EOT
+  },
+  {
+    :id => 3,
+    :title => 'how can i get up early?',
+    :content => <<~EOT
+    test test
+  EOT
+  },
+  {
+    :id => 4,
+    :title => 'hogehoge',
+    :content => <<~EOT
+    aaaa aaaaaaaaaaaaa aaaaaaaaaaaa aaaa aaa aaaaaaaaaaaaaa aaaaaaaa
+  EOT
+  },
+  {
+    :id => 5,
+    :title => 'fugafuga',
+    :content => <<~EOT
+    aaaa aaaaaaaaaaaaa aaaaaaaaaaaa aaaa aaa aaaaaaaaaaaaaa aaaaaaaa
+  EOT
+  },
+  {
+    :id => 6,
+    :title => 'piyopiyo',
+    :content => <<~EOT
+    aaaa aaaaaaaaaaaaa aaaaaaaaaaaa aaaa aaa aaaaaaaaaaaaaa aaaaaaaa
+  EOT
+  }
+)
